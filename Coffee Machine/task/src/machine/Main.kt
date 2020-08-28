405c3b36a644428e86e76b8fc4803e9ad57f4511
Aleksey Glyantsev <aglyancev@gmail.com>
2020-08-28 19:52:37 +0300

    Stage #4: Action!
    Description

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -2,51 +2,104 @@ package machine
 
 import java.util.*
 
-fun main() {
-    val scanner = Scanner(System.`in`)
-    val water = 200
-    val milk = 50
-    val coffeeBeans = 15
-    var minCups = 0
+val scanner = Scanner(System.`in`)
+var water = 400
+var milk = 540
+var coffeeBeans = 120
+var disposableCups = 9
+var money = 550
 
+fun main() {
+    coffeeMachineHas()
+    print("Write action (buy, fill, take): ")
+    val action = scanner.next()
+    when(action) {
+
+        "buy" -> buy()
+        "fill" -> fill()
+        "take" -> take()
+    }
+}
+
+fun buy(){
+
+    val espressoWater = 250
+    val espressoBeans = 16
+    val espressoCost = 4
+
+    val latteWater = 350
+    val latteMilk = 75
+    val latteBeans = 20
+    val latteCost = 7
+
+    val cappuccinoWater = 200
+    val cappuccinoMilk = 100
+    val cappuccinoBeans = 12
+    val cappuccinoCost = 6
+
+    print("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    when(scanner.nextInt()){
+        1 -> {
+            println("The coffee machine has:")
+            println("${water - espressoWater} of water")
+            println("${milk} of milk")
+            println("${coffeeBeans - espressoBeans} of coffee beans")
+            println("${disposableCups - 1} of disposable cups")
+            println("${money + espressoCost} of money")
+        }
+        2 -> {
+            println("The coffee machine has:")
+            println("${water - latteWater} of water")
+            println("${milk - latteMilk} of milk")
+            println("${coffeeBeans - latteBeans} of coffee beans")
+            println("${disposableCups - 1} of disposable cups")
+            println("${money + latteCost} of money")
+        }
+        3 -> {
+            println("The coffee machine has:")
+            println("${water - cappuccinoWater} of water")
+            println("${milk - cappuccinoMilk} of milk")
+            println("${coffeeBeans - cappuccinoBeans} of coffee beans")
+            println("${disposableCups - 1} of disposable cups")
+            println("${money + cappuccinoCost} of money")
+        }
+    }
+}
+
+fun fill(){
     print("Write how many ml of water the coffee machine has: ")
     val hasWater = scanner.nextInt()
 
     print("\nWrite how many ml of milk the coffee machine has: ")
     val hasMilk = scanner.nextInt()
 
-    print("\nWrite how many grams of coffee beans the coffee machine has: ")
+    print("\nWrite how many disposable cups of coffee do you want to add ")
     val hasCoffeeBeans = scanner.nextInt()
 
     print("\nWrite how many cups of coffee you will need: ")
-    val cupsOfCoffeeNeeds = scanner.nextInt()
+    val hasDisposableCups = scanner.nextInt()
 
-    val minWater = hasWater/water
-    val minMilk = hasMilk/milk
-    val minCoffeeBeans = hasCoffeeBeans/coffeeBeans
+    println("\n${water + hasWater} of water")
+    println("${milk + hasMilk} of milk")
+    println("${coffeeBeans + hasCoffeeBeans} of coffee beans")
+    println("${disposableCups + hasDisposableCups} of disposable cups")
+    println("$money of money")
+}
 
-    minCups += if ((minMilk in minCoffeeBeans..minWater) || (minWater in minCoffeeBeans..minMilk)) {
-        minCoffeeBeans
-    } else if ((minCoffeeBeans in minMilk..minWater) || (minWater in minMilk..minCoffeeBeans)){
-        minMilk
-    } else {
-        minWater
-    }
+fun take(){
+    println("I gave you $$money")
+    println("\n$water of water")
+    println("$milk of milk")
+    println("$coffeeBeans of coffee beans")
+    println("$disposableCups of disposable cups")
+    println("${money - money} of money")
+}
 
-    when {
-        minCups == cupsOfCoffeeNeeds -> {
-            println("Yes, I can make that amount of coffee")
-        }
-        minCups < cupsOfCoffeeNeeds && cupsOfCoffeeNeeds != 0 -> {
-            println("No, I can make only $minCups cups of coffee")
-        }
-        minCups > cupsOfCoffeeNeeds && cupsOfCoffeeNeeds != 0 -> {
-            println("Yes, I can make that amount of coffee (and even " +
-                    "${cupsOfCoffeeNeeds - minCups} more than that)")
-        }
-        cupsOfCoffeeNeeds <= 0 -> {
-            println("Yes, I can make that amount of coffee (and even " +
-                    "1 more than that)")
-        }
-    }
+fun coffeeMachineHas(){
+    println("The coffee machine has:")
+    println("$water of water")
+    println("$milk of milk")
+    println("$coffeeBeans of coffee beans")
+    println("$disposableCups of disposable cups")
+    println("$money of money\n")
 }
