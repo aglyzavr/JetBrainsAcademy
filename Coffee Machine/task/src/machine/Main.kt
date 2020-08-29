96df6d33b464f8a45557d469a53dff4123ad4b16
Aleksey Glyantsev <aglyancev@gmail.com>
2020-08-29 15:22:57 +0300

    Stage #5: On a coffee loop
    Description

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,6 +1,7 @@
 package machine
 
 import java.util.*
+import kotlin.system.exitProcess
 
 val scanner = Scanner(System.`in`)
 var water = 400
@@ -10,14 +11,15 @@ var disposableCups = 9
 var money = 550
 
 fun main() {
-    coffeeMachineHas()
-    print("Write action (buy, fill, take): ")
+    print("Write action (buy, fill, take, remaining, exit): ")
     val action = scanner.next()
     when(action) {
 
         "buy" -> buy()
         "fill" -> fill()
         "take" -> take()
+        "remaining" -> coffeeMachineHas()
+        "exit" -> exitProcess(0)
     }
 }
 
@@ -38,36 +40,78 @@ fun buy(){
     val cappuccinoCost = 6
 
     print("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
-    when(scanner.nextInt()){
-        1 -> {
-            println("The coffee machine has:")
-            println("${water - espressoWater} of water")
-            println("${milk} of milk")
-            println("${coffeeBeans - espressoBeans} of coffee beans")
-            println("${disposableCups - 1} of disposable cups")
-            println("${money + espressoCost} of money")
+    when(scanner.next()){
+        "1" -> {
+            if (water >= espressoWater && coffeeBeans >= espressoBeans && disposableCups != 0){
+                println("I have enough resources, making you a coffee!\n")
+                water -= espressoWater
+                coffeeBeans -= espressoBeans
+                disposableCups -= 1
+                money += espressoCost
+            } else if (water < espressoWater){
+                println("I have not enough water!\n")
+                main()
+            } else if (coffeeBeans < espressoBeans){
+                println("I have not enough coffee beans!")
+                main()
+            } else if (disposableCups == 0){
+                println("I have not enough disposable cups!")
+                main()
+            }
         }
-        2 -> {
-            println("The coffee machine has:")
-            println("${water - latteWater} of water")
-            println("${milk - latteMilk} of milk")
-            println("${coffeeBeans - latteBeans} of coffee beans")
-            println("${disposableCups - 1} of disposable cups")
-            println("${money + latteCost} of money")
+        "2" -> {
+
+            if (water >= latteWater && milk > latteMilk  && coffeeBeans >= espressoBeans && disposableCups != 0){
+                println("I have enough resources, making you a coffee!\n")
+                water -= latteWater
+                milk -= latteMilk
+                coffeeBeans -= latteBeans
+                disposableCups -= 1
+                money += latteCost
+            } else if (water < latteWater){
+                println("I have not enough water!\n")
+                main()
+            } else if (milk < latteMilk){
+                println("I have not enough water!\n")
+                main()
+            }else if (coffeeBeans < latteBeans){
+                println("I have not enough coffee beans!\n")
+                main()
+            } else if (disposableCups == 0){
+                println("I have not enough disposable cups!\n")
+                main()
+            }
         }
-        3 -> {
-            println("The coffee machine has:")
-            println("${water - cappuccinoWater} of water")
-            println("${milk - cappuccinoMilk} of milk")
-            println("${coffeeBeans - cappuccinoBeans} of coffee beans")
-            println("${disposableCups - 1} of disposable cups")
-            println("${money + cappuccinoCost} of money")
+        "3" -> {
+            if (water >= cappuccinoWater && milk > cappuccinoMilk  && coffeeBeans >= cappuccinoBeans && disposableCups != 0){
+                println("I have enough resources, making you a coffee!\n")
+                water -= cappuccinoWater
+                milk -= cappuccinoMilk
+                coffeeBeans -= cappuccinoBeans
+                disposableCups -= 1
+                money += cappuccinoCost
+            } else if (water < cappuccinoWater){
+                println("I have not enough water!\n")
+                main()
+            } else if (milk < cappuccinoMilk){
+                println("I have not enough water!\n")
+                main()
+            }else if (coffeeBeans < cappuccinoBeans){
+                println("I have not enough coffee beans!\n")
+                main()
+            } else if (disposableCups == 0){
+                println("I have not enough disposable cups!\n")
+                main()
+            }
         }
+        "back" -> main()
     }
+
+    main()
 }
 
 fun fill(){
-    print("Write how many ml of water the coffee machine has: ")
+    print("\nWrite how many ml of water the coffee machine has: ")
     val hasWater = scanner.nextInt()
 
     print("\nWrite how many ml of milk the coffee machine has: ")
@@ -79,27 +123,28 @@ fun fill(){
     print("\nWrite how many cups of coffee you will need: ")
     val hasDisposableCups = scanner.nextInt()
 
-    println("\n${water + hasWater} of water")
-    println("${milk + hasMilk} of milk")
-    println("${coffeeBeans + hasCoffeeBeans} of coffee beans")
-    println("${disposableCups + hasDisposableCups} of disposable cups")
-    println("$money of money")
+    water += hasWater
+    milk += hasMilk
+    coffeeBeans += hasCoffeeBeans
+    disposableCups +=hasDisposableCups
+
+    main()
 }
 
 fun take(){
-    println("I gave you $$money")
-    println("\n$water of water")
-    println("$milk of milk")
-    println("$coffeeBeans of coffee beans")
-    println("$disposableCups of disposable cups")
-    println("${money - money} of money")
+    println("I gave you $$money\n")
+    money -= money
+
+    main()
 }
 
 fun coffeeMachineHas(){
-    println("The coffee machine has:")
+    println("\nThe coffee machine has:")
     println("$water of water")
     println("$milk of milk")
     println("$coffeeBeans of coffee beans")
     println("$disposableCups of disposable cups")
     println("$money of money\n")
+
+    main()
 }
